1b47171d83b2f90e99188489af63030ce009e282
YanChii <janci@binaryparadise.com>
2019-10-16 17:27:06 +0000

    bump version to v4.2

--- user-guide/conf.py
+++ user-guide/conf.py
@@ -27,7 +27,7 @@ sys.path.append(abspath(join(dirname(__file__), "_ext")))
 try:
     from core.version import __version__ as VERSION
 except ImportError:
-    VERSION = '4.1'
+    VERSION = '4.2'
 
 # -- General configuration -----------------------------------------------------
 
